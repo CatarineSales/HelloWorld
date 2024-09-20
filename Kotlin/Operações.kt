c4dd59071e2113523cc6d74fe5ee27f23d2e677a
Catarine <126796384+CatarineSales@users.noreply.github.com>
2024-09-20 00:44:47 -0300

    Create Operações.kt

---
+++ Kotlin/Operações.kt
@@ -0,0 +1,12 @@
+fun main(){
+  var a:Int = 7
+  var b:Int = 3
+
+  val soma = a + b
+  val subtração = a - b
+  val multiplicacao = a * b
+  val divisao = a / b
+  val modulo = a % 2
+
+  println("$soma $subtração $multiplicacao $divisao $modulo")
+}
